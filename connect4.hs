5842e812a2edf6ccece73a2f45d251ee02f6b231
opbokel <dobra9@gmail.com>
2020-08-27 21:49:35 +0200

    Two functions left

--- connect4.hs
+++ connect4.hs
@@ -48,7 +48,7 @@ nextFree colIndex board =
 placePiece :: Int -> State -> Maybe (State, Status)
 placePiece colIndex state = do  
      coord     <- nextFree colIndex (board state)
-     nextBoard <- M.safeSet (num (player state)) coord (board state)
+     nextBoard <- M.safeSet (num $ player state) coord (board state)
      let status = getStatus coord (State (player state) nextBoard) -- Current player turn after placing the piece
      return (State (next (player state)) nextBoard, status) 
 
@@ -64,37 +64,61 @@ getStatus coord state
     | checkFull (board state) = Draw
     | otherwise = Running
 
+-- WARNING: Side effects beyond this point!
 
 main :: IO ()
 main = do
-    putStrLn ("Functional Connect " ++ show toWinLength ++ "\nAt any time, enter q to quit or r to restart:")
+    putStrLn $ "Functional Connect " ++ show toWinLength ++ "\nAt any time, enter q to quit or r to restart:"
     play (State Player1 startBoard)                                
     
 printBoard :: State -> IO ()
 printBoard state = do 
-    putStrLn (show boardColIndex)
-    putStrLn (show (board state))
+    putStrLn $ show boardColIndex
+    putStrLn $ show (board state)
 
 play :: State -> IO ()
 play state = do
     printBoard state
-    putStrLn ((show (player state)) ++ " Next move (1 to " ++ (show boardWidth) ++ ") ?")
+    putStrLn $ (show $ player state) ++ " Next move (1 to " ++ (show boardWidth) ++ ") ?"
     line <- getLine
     processLine line state
 
 processLine :: [Char] -> State -> IO ()
-processLine "r" _      = main
+processLine "r" _      = startNewGame
 processLine "q" _      = putStrLn "The game will end now, have a very functional day!"
-processLine line state = proccessMaybeColIndex (readMaybe line :: Maybe Int) state
+processLine line state = proccessColIndex (readMaybe line :: Maybe Int) state
 
-proccessMaybeColIndex :: Maybe Int -> State -> IO ()
-proccessMaybeColIndex Nothing state = do
+startNewGame :: IO ()
+startNewGame = do
+    putStrLn "Will start a new game!\n"
+    main
+
+proccessColIndex :: Maybe Int -> State -> IO ()
+proccessColIndex Nothing state = do
     putStrLn "Invalid number, please try again."
     play state
 
-proccessMaybeColIndex (Just colIndex) state = putStrLn "Just to compile"
+proccessColIndex (Just colIndex) state = processNextState state (placePiece colIndex state)
 
 
+processNextState :: State -> Maybe (State, Status) -> IO ()
+processNextState currentState Nothing = do
+    putStrLn "Invalid column number. Please verify and try again:"
+    putStrLn  $ " - if your value is between 1 and " ++ (show boardWidth)
+    putStrLn " - if the column is not full "
+    play currentState
+
+processNextState _ (Just (newState, Running)) = play newState
+
+processNextState _ (Just (newState, Draw)) = do
+    printBoard newState
+    putStrLn "No losers today, the game is Draw!"
+    startNewGame
+
+processNextState currentState (Just (newState, Victory)) = do
+    printBoard newState
+    putStrLn $ (show $ player currentState) ++ " is the most functional player and the winner!"
+    startNewGame
 
 
 
