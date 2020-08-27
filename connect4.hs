5bdb91864f66a756fce9a05ff0c2db93cec3013b
opbokel <dobra9@gmail.com>
2020-08-27 19:17:20 +0200

    A little update

--- connect4.hs
+++ connect4.hs
@@ -1,6 +1,7 @@
 import qualified Data.Matrix as M
 import qualified Data.Vector as V
 import Data.Maybe
+import Text.Read
 
 boardWidth = 7
 boardHeight = 6
@@ -8,11 +9,10 @@ toWinLength = 4
 
 data Status = Draw | Victory | Running deriving Eq
 
-data Player = Player1 | Player2 deriving Eq
+data Player = Player1 | Player2 deriving (Eq, Enum)
 
 num :: Player -> Int
-num Player1 = 1
-num Player2 = 2
+num player = (fromEnum player) + 1
 
 instance Show Player where
     show player = "Player " ++ show (num player)
@@ -25,6 +25,8 @@ type Board = M.Matrix Int
 
 type Coord = (Int, Int)
 
+data State = State { player :: Player, board :: Board }
+
 boardColIndex = M.fromList 1 boardWidth [1 .. boardWidth]
 
 startBoard :: Board
@@ -32,57 +34,68 @@ startBoard = M.zero boardHeight boardWidth
 
 isUsed value = value /= 0
 
-validOrNothing 0 = Nothing
-validOrNothing colIndex = Just colIndex
+validRowOrNothing :: Int -> Maybe Int
+validRowOrNothing rowIndex = if (rowIndex < 1 || rowIndex > boardHeight) then Nothing else Just rowIndex 
 
 nextFreeInColumn :: V.Vector Int -> Maybe Int
-nextFreeInColumn column = validOrNothing (fromMaybe (V.length column) (V.findIndex isUsed column))
+nextFreeInColumn column = validRowOrNothing (fromMaybe (V.length column) (V.findIndex isUsed column))
 
 -- Matrix index starts at 1
 nextFree :: Int -> Board -> Maybe Coord
 nextFree colIndex board = 
     fmap (\rowIndex -> (rowIndex, colIndex)) ((M.safeGetCol colIndex board) >>= nextFreeInColumn)
 
-placePiece :: Player -> Int -> Board -> Maybe (Board, Status)
-placePiece player colIndex board = do  
-     coord     <- nextFree colIndex board
-     nextBoard <- M.safeSet (num player) coord board
-     let status = getStatus player coord nextBoard 
-     return (nextBoard, status) 
+placePiece :: Int -> State -> Maybe (State, Status)
+placePiece colIndex state = do  
+     coord     <- nextFree colIndex (board state)
+     nextBoard <- M.safeSet (num (player state)) coord (board state)
+     let status = getStatus coord (State (player state) nextBoard) -- Current player turn after placing the piece
+     return (State (next (player state)) nextBoard, status) 
 
-checkWin :: Player -> Coord -> Board -> Bool
-checkWin player coord board = False -- To be done
+checkWin :: Coord -> State -> Bool
+checkWin coord state = False -- To be done
 
 checkFull :: Board -> Bool
 checkFull board = False -- To be done
 
-getStatus :: Player -> Coord -> Board -> Status
-getStatus player coord board 
-    | checkWin player coord board = Victory
-    | checkFull board = Draw
+getStatus :: Coord -> State -> Status
+getStatus coord state 
+    | checkWin coord state = Victory
+    | checkFull (board state) = Draw
     | otherwise = Running
 
 
 main :: IO ()
 main = do
     putStrLn ("Functional Connect " ++ show toWinLength ++ "\nAt any time, enter q to quit or r to restart:")
-    play Player1 startBoard                                
+    play (State Player1 startBoard)                                
     
-
-play :: Player -> Board -> IO ()
-play player board = do
+printBoard :: State -> IO ()
+printBoard state = do 
     putStrLn (show boardColIndex)
-    putStrLn (show board)
-    putStrLn ((show player) ++ " next move (1 to " ++ (show boardWidth) ++ ") ?")
+    putStrLn (show (board state))
+
+play :: State -> IO ()
+play state = do
+    printBoard state
+    putStrLn ((show (player state)) ++ " Next move (1 to " ++ (show boardWidth) ++ ") ?")
     line <- getLine
-    readLine line
+    processLine line state
 
-readLine :: [Char] -> IO ()
-readLine "r" = main
-readLine "q" = putStrLn "The game will end now!"
-readLine   x = return ()
+processLine :: [Char] -> State -> IO ()
+processLine "r" _      = main
+processLine "q" _      = putStrLn "The game will end now, have a very functional day!"
+processLine line state = proccessMaybeColIndex (readMaybe line :: Maybe Int) state
 
--- readLetter l = IO ()
+proccessMaybeColIndex :: Maybe Int -> State -> IO ()
+proccessMaybeColIndex Nothing state = do
+    putStrLn "Invalid number, please try again."
+    play state
+
+proccessMaybeColIndex (Just colIndex) state = putStrLn "Just to compile"
+
+
+
 
 
 
